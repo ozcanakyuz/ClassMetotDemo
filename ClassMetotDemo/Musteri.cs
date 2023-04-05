bdc27494705cf63aff3911d636976633d958a973
ozcanakyuz <xe_bilmece@hotmail.com>
2023-04-05 18:26:02 +0300

    Add project files.

---
+++ ClassMetotDemo/Musteri.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    internal class Musteri
+    {
+        public int Id { get; set; }
+        public int TcNo { get; set; }
+        public int Adi { get; set; }
+        public int Soyadi { get; set; }
+        public int Bakiye { get; set; }
+    }
+}
